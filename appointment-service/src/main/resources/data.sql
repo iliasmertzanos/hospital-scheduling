82236a911cee37f753190b98cd992d781758840b
Ilias Mertzanidis <mertzanos@gmail.com>
2020-05-10 10:26:19 +0200

    appointment-service: column name disease changed (from dicease) due to tipo

--- appointment-service/src/main/resources/data.sql
+++ appointment-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO patient (id, age, dicease, height, insurance_id, name, weight) VALUES (2, 26, 'DIAB_TYP_1', 200.0, 6191030, 'Ilias Mertzanidis', 90.0);
-INSERT INTO patient (id, age, dicease, height, insurance_id, name, weight) VALUES (3, 26, 'DIAB_TYP_1', 200.0, 6191030, 'Georgios Mertzanidis', 90.0);
-INSERT INTO patient (id, age, dicease, height, insurance_id, name, weight) VALUES (4, 26, 'DIAB_TYP_1', 200.0, 6191030, 'Ioli Mertzanidis', 90.0);
+INSERT INTO patient (id, age, disease, height, insurance_id, name, weight) VALUES (2, 26, 'DIAB_TYP_1', 200.0, 6191030, 'Ilias Mertzanidis', 90.0);
+INSERT INTO patient (id, age, disease, height, insurance_id, name, weight) VALUES (3, 26, 'DIAB_TYP_1', 200.0, 6191030, 'Georgios Mertzanidis', 90.0);
+INSERT INTO patient (id, age, disease, height, insurance_id, name, weight) VALUES (4, 26, 'DIAB_TYP_1', 200.0, 6191030, 'Ioli Mertzanidis', 90.0);
